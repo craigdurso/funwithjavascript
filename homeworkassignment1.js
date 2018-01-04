658c1a4d9f89a6e8967c882a3c427d818564c932
Craig Durso <craig.robert.durso@gmail.com>
2018-01-03 20:14:02 -0500

    fun with javascript

---
+++ homeworkassignment1.js
@@ -0,0 +1,58 @@
+var x = 50
+var y = 37
+
+var result = x + y
+console.log(x);
+console.log("x");
+console.log(y);
+console.log(result);
+console.log(x+y);
+
+function addition(x, y){
+  return x + y
+}
+
+console.log(addition(x,y));
+
+
+
+
+var cars = ["Infiniti", "Jeep", "Lexus", "Mercedes"]
+var colors = ["red", "black", "white", "silver"]
+var carType = ["coupe", "suv", "sedan"]
+
+console.log(cars[1]+colors[3]+carType[2])
+
+
+
+
+var number = 125
+
+if(number<100){
+  alert ("your variable is less than 100")
+} else{
+  alert("your number,"+ number + ",is greater than or equal to 100 :)");
+}
+
+
+
+
+var door1
+
+function doorgame(door1, door2, door3){
+  return door1
+  return door2
+  return door3
+  return door4
+}
+  if (door1)
+  {console.log;("you win the grand prize")
+}else if(door2)
+  {console.log;("you win a new car")
+}else(door3)
+  {console.log;("no luck")
+}else(door4)
+  {console.log;("win a free lunch")}
+
+doorgame(door1)
+alert(result);
